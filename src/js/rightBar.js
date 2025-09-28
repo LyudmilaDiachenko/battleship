a74411d82b0c46b1fcb3764a3824d9b8d4f80fa3
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-28 17:38:29 +0300

    restart button

--- src/js/rightBar.js
+++ src/js/rightBar.js
@@ -1,26 +1,8 @@
 import React from "react";
 
-function RightBar({reset, theme, setTheme}){
-  function generateThemes(){
-    const themesCount = 8
-    const squeare = Math.ceil(Math.sqrt(themesCount + 1))
-    let divatoz = []
-    for(let i = 0; i<=themesCount; i++){
-      divatoz.push(
-        <>
-          <input key={"theme-check-"+i} id={"check-"+i} type="radio" checked={theme===i} onChange={_=>setTheme(i)} />
-          <label key={"theme-cell-"+i} htmlFor={"check-"+i} className={"cell theme-"+i} style={{backgroundImage: `url("/media/${i}.jpg")`, width: 100/squeare + "%"}} />
-        </>
-      )
-    }
-    return divatoz
-  }
-
+function RightBar({reset}){
   return (
     <div className="aside aside-right">
-        <div className="themes">
-          {generateThemes()}
-        </div>
           <div>
             <button onClick={reset}>
               Почати заново
